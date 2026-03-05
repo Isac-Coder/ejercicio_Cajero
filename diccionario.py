70ea06302d6e8f3ed55402029e1b20a1be913d39
Nesdael <danieldurfue7@gmail.com>
2026-03-04 19:00:18 -0500

    diccionario de usuarios

---
+++ diccionario.py
@@ -0,0 +1,18 @@
+CUENTAS = {
+    
+    "isac" : { 
+        "clave" : "1234",
+        "saldo" : 1000
+    },
+
+    "abraham" : {
+        "clave" : "2222",
+        "saldo" : 1000
+    },
+    
+    "luis" : {
+        "clave" : "3333",
+        "saldo" : 1000
+    }
+    
+} 
