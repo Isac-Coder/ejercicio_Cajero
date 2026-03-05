f863e5c45dab627fc70a9da7ca79f37325ea9a90
Isac <isaacalvarez0516@gmail.com>
2026-03-05 17:48:08 -0500

    validaciones

--- depositar_dinero.py
+++ depositar_dinero.py
@@ -2,12 +2,13 @@ from colores import *
 from tiempo_de_carga import cargar
 from limpiar_pantalla import Limpiar_pantalla
 from main import saldo
-
+from Validaciones import pedir_numero
 while True:
     def depositar_dinero():
         Limpiar_pantalla()
         global saldo
-        monto = float(input(f"{VERDE}Ingrese el monto a depositar:{BLANCO}\n$ "))
+        
+        monto = pedir_numero(f"{VERDE}Ingrese el monto a depositar:{BLANCO}\n$ ")
         print(f"{VERDE}")
 
         Limpiar_pantalla()
@@ -15,15 +16,8 @@ while True:
         print("\n")
         Limpiar_pantalla()
 
-        if monto != int:
-                print(f"{ROJO}⚠️  El monto ingresado no es un número válido. Por favor, ingrese un monto numérico. ⚠️\n{VERDE}")
-                input(f"{AMARILLO}Presione Enter para continuar...")
-    
-                Limpiar_pantalla()
-                cargar()
-                Limpiar_pantalla()
-                return
-        elif monto <= 0:
+
+        if monto <= 0:
             print(f"{ROJO}⚠️  El monto no puede ser un numero negativo o cero. ⚠️\n{VERDE}")
             print("Por favor, ingrese un monto válido.\n")
             input(f"{AMARILLO}Presione Enter para continuar...")
