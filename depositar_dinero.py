ebbf1adb58b48a37c8ddce81a2e02f15107e9691
Isac-Coder <isaacalvarez0516@gmail.com>
2026-03-05 22:50:26 -0500

    casi arreglado funcion usuarios

--- depositar_dinero.py
+++ depositar_dinero.py
@@ -2,14 +2,17 @@ from ansi_colores import *
 from tiempo_de_carga import cargar
 from limpiar_pantalla import Limpiar_pantalla
 from Validaciones import pedir_numero
-from usuarios import *
+from usuarios import CUENTAS
 from login import *
 
 
 
-def depositar_dinero(usuario, CUENTAS):
-        
-        monto = pedir_numero(f"{VERDE}Ingrese el monto a depositar:{BLANCO}\n$ ")
+def depositar_dinero(usuarios, CUENTAS):
+        Limpiar_pantalla()
+        cargar()
+        Limpiar_pantalla()
+        print("\n")
+        monto_deposito = pedir_numero(f"{VERDE}Ingrese el monto a depositar:{BLANCO}\n$ ")
         print(f"{VERDE}")
         Limpiar_pantalla()
         cargar()
@@ -17,7 +20,7 @@ def depositar_dinero(usuario, CUENTAS):
         Limpiar_pantalla()
 
 
-        if monto <= 0:
+        if monto_deposito <= 0:
             print(f"{ROJO}⚠️  El monto no puede ser un numero negativo o cero. ⚠️\n{VERDE}")
             print("Por favor, ingrese un monto válido.\n")
             input(f"{AMARILLO}Presione Enter para continuar...")
@@ -26,7 +29,7 @@ def depositar_dinero(usuario, CUENTAS):
             cargar()
             Limpiar_pantalla()
         else:
-            if monto < 100:
+            if monto_deposito < 100:
                 print(f"{ROJO}⚠️  El monto minimo permitido para depósito es de {BLANCO}$100.{ROJO} Intente nuevamente. ⚠️\n{VERDE}")
                 input(f"{AMARILLO}Presione Enter para continuar...")
 
@@ -34,13 +37,24 @@ def depositar_dinero(usuario, CUENTAS):
                 cargar()
                 Limpiar_pantalla()
                 return
-            CUENTAS[usuario]["saldo"] += monto
+"""            CUENTAS[usuario]["saldo"] += monto_deposito
             print(f"✅ Depósito exitoso. Nuevo saldo:\n")
             print(f"{BLANCO}$ {int(CUENTAS[usuario]['saldo'])}{VERDE}\n")
             input(f"{AMARILLO}Presione Enter para continuar...{VERDE}")
+"""
+        if usuario in CUENTAS["usuarios"]:
 
-            Limpiar_pantalla()
-            cargar()
-            Limpiar_pantalla()
+            if "saldo" not in CUENTAS["usuarios"][usuario]:
+                CUENTAS["usuarios"][usuario]["saldo"] = 0
 
-    
+            CUENTAS["usuarios"][usuario]["saldo"] += monto_deposito
+
+            print(f"✅ Depósito exitoso. Nuevo saldo:\n")
+            print(f"{BLANCO}$ {int(CUENTAS['usuarios'][usuario]['saldo'])}{VERDE}\n")
+
+            input(f"{AMARILLO}Presione Enter para continuar...")
+
+        else:
+            print(f"{ROJO}El usuario no existe.{BLANCO}")
+
+depositar_dinero("usuario", CUENTAS)
